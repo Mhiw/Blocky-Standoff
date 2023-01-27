f34d04b7f2617f1bd7266a73364c5250c6994c0c
Mhiw <wil.carlsson@gmail.com>
2023-01-27 20:23:26 +0100

    Movement is working, timer is working

--- Blocky Standoff/Assets/Scripts/Movement.cs
+++ Blocky Standoff/Assets/Scripts/Movement.cs
@@ -6,9 +6,22 @@ public class Movement : MonoBehaviour
 {
     [Header("Refrences")]
     [SerializeField] private GameObject OtherPlayer;
+    [SerializeField] private Rigidbody2D rb;
 
-    void Update()
+    [Header("Stats")]
+    [SerializeField] private float speed = 10;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
     {
         OtherPlayer.transform.position = new Vector2(-transform.position.x, -transform.position.y);
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(horizontalInput * speed, verticalInput * speed);
     }
 }
